a6041b18ca032ac6cab31fd64255d12276cf4020
GNcube <4821668+gncube@users.noreply.github.com>
2023-01-30 19:24:37 +0000

    Add project files.

---
+++ CodilitySolutions.Core/Codility.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+public partial class Codility
+{
+    public Codility()
+    {
+    }
+
+    public void MinAvgTwoSlices()
+    {
+        Console.WriteLine("---> Codility_ Problem ");
+
+        var sw = Stopwatch.StartNew();
+
+        sw.Stop();
+        Console.WriteLine($" Exceution time: {sw.ElapsedMilliseconds}");
+
+        Console.WriteLine($"---> The solution is ");
+    }
+}
---
+++ Console.UI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+string s;
+
+do
+{
+    Console.WriteLine("---> Codility_   ");
+    s = Console.ReadLine();
+
+    if (!String.IsNullOrWhiteSpace(s))
+    {
+        appMenu(s.Trim());
+    }
+
+} while (true); //Space & Enter Kill the process.
+
+static void appMenu(string codilityId)
+{
+    Codility codility = new();
+
+    switch (codilityId)
+    {
+        case "5":
+            codility.MinAvgTwoSlices();
+            break;
+        default:
+            Console.WriteLine("There is no solution for the problem # {0} yet. Please try it later. Thanks! \n", codilityId);
+            break;
+    }
+}
+
+Process.GetCurrentProcess().Kill();
